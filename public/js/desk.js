48bf1fd6bf1c6c2958e4070e9fbe670d9869b906
Gigiox <guillermo.casanova.b@gmail.com>
2022-03-13 21:01:38 -0300

    Preparing desk screen

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,3 +1,35 @@
+// HTML References
+const lblDesk = document.querySelector('h1');
+const btnAttend = document.querySelector('button');
 
+const searchParams = new URLSearchParams( window.location.search );
 
-console.log('Desk HTML');
+if ( !searchParams.has('desk') ) {
+    window.location = 'index.html';
+    throw new Error('Desk is required');
+}
+
+const desk = searchParams.get('desk');
+lblDesk.innerText = desk;
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnAttend.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAttend.disabled = true;
+});
+
+socket.on('last-ticket', ( last ) => {
+    // lblNewTicket.innerText = 'Ticket ' + last;
+})
+
+btnAttend.addEventListener( 'click', () => {
+    
+    socket.emit( 'next-ticket', null, ( ticket ) => {
+        lblNewTicket.innerText = ticket;
+    });
+
+});
